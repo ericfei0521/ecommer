8bf239766411cdee2b8da2c08834ea33439e0b80
ericfei306 <eric0521bigjohn@gmail.com>
2020-06-14 23:05:16 +0800

    update menu-item to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import{Route}from'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 
 
-class App extends React.Component {
-  constructor() {
-    super();
+const HatsPage =()=>(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
-    this.state = {
-      showChild: true,
-      text: ''
-    };
-  }
-
-  render() {
-    return (
-      <div >
-        <HomePage/>
-      </div>
-    );
-  }
+function App(){
+  return(
+    <div>
+      <switch>
+      <Route  exact path='/' component={HomePage}/>
+      <Route  path='/hats' component={HatsPage}/>        
+      </switch>
+    </div>
+  )
 }
+// class App extends React.Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       showChild: true,
+//       text: ''
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div >
+//         <HomePage/>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
